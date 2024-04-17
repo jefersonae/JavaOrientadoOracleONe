3bbc5733db18a7c0ea57c8bda0e63f39028b09ff
Jeferson de Souza Andrade <38109358+jefersonae@users.noreply.github.com>
2024-04-17 01:13:15 -0300

    Projeto Final de OO

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);;
+
+        meuFilme.exibeFichaTecnica();
+    }
+}
